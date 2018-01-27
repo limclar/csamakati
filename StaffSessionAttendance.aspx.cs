9b6cdc6499b8c742202538b39f3ff59a04fc8812
Jon Pardilla <akcpardilla@gmail.com>
2018-01-27 14:31:38 +0800

    Update StaffSessionAttendance.aspx.cs

--- StaffSessionAttendance.aspx.cs
+++ StaffSessionAttendance.aspx.cs
@@ -93,7 +93,7 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnCancelSession_Click(object sender, EventArgs e)
     {                                                                                                           
-        ScriptManager.RegisterStartupScript(this, typeof(string), "Message", "var confirm_value = document.createElement('INPUT');confirm_value.type = 'hidden';confirm_value.name = 'confirm_value'; if(confirm('Do you really want to cancel the consultation?')){confirm_value.value = 'Yes';alert('Consultation has been cancelled!');}else{confirm_value.value = 'No';}document.forms[0].appendChild(confirm_value);",true);
+        ScriptManager.RegisterStartupScript(this, typeof(string), "Message", "var confirm_value = document.createElement('INPUT');confirm_value.type = 'hidden';confirm_value.name = 'confirm_value'; if(confirm('Do you really want to cancel the consultation?')){confirm_value.value = 'Yes';}else{confirm_value.value = 'No';} document.forms[0].appendChild(confirm_value);",true);
         string confirmValue = Request.Form["confirm_value"];
         if(confirmValue == "YES")
         {
@@ -106,7 +106,8 @@ public partial class _Default : System.Web.UI.Page
                 string studCNumber = Class2.getSingleData("SELECT dbo.Student.Contact + ';' + CONVERT(nvarchar, ConsultationDate) + ';' + CONVERT(nvarchar, TimeStart, 120) + ';' FROM dbo.PeerAdviserConsultations INNER JOIN dbo.Student ON dbo.PeerAdviserConsultations.StudentNumber = dbo.Student.StudentNumber WHERE PConsultationId = " + Request.QueryString["aId"]);
                 msg("0" + studCNumber.Split(';')[0].ToString(), "Your appointment on " + studCNumber.Split(';')[1].ToString() + " " + studCNumber.Split(';')[2].ToString() + " has been cancelled.", "ST-CLARE459781_FISP7");     
             }
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('You have been inactive for too long. Please relogin.');window.location ='Out.aspx';", true);
+            
+            this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Consultation has been cancelled!')", true);
         }
     }
 }
