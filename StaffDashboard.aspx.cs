5632e734eabec0e93e62e324b758711a1db6a696
Jon Pardilla <akcpardilla@gmail.com>
2018-01-27 22:19:26 +0800

    Update StaffDashboard.aspx.cs

--- StaffDashboard.aspx.cs
+++ StaffDashboard.aspx.cs
@@ -51,6 +51,7 @@ public partial class _Default : System.Web.UI.Page
             Session["conType"] = "AND (ConsultationType = 'Walk-in')";
             
         populateBtn();
+        Session["queryRange"] = Session["queryRange"].toString().Split('AND')[0] + Session["conType"];
         BindGvData();
         BindChart();
     }
