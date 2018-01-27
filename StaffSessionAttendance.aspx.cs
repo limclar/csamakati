106b513c87b648f08bacbab8509f55b5f5d8901b
Jon Pardilla <akcpardilla@gmail.com>
2018-01-27 14:09:06 +0800

    Update StaffSessionAttendance.aspx.cs

--- StaffSessionAttendance.aspx.cs
+++ StaffSessionAttendance.aspx.cs
@@ -93,7 +93,9 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnCancelSession_Click(object sender, EventArgs e)
     {
-        ScriptManager.RegisterStartupScript(this, typeof(string), "Message", "if(confirm('Do you really want to cancel the consultation?')){alert('Consultation has been cancelled!')"<%#Session["Confirm"] = 0; %>";window.close();}else{ "<%#Session["Confirm"] = 1; %>" }",true);
+        ScriptManager.RegisterStartupScript(this, typeof(string), "Message", "var confirm_value = document.createElement("INPUT");
+            confirm_value.type = "hidden";
+            confirm_value.name = "confirm_value"; if(confirm('Do you really want to cancel the consultation?')){confirm_value.value = "Yes";;alert('Consultation has been cancelled!')"<%#Session["Confirm"] = 0; %>";window.close();}else{ confirm_value.value = "No"; }",true);
         SqlCommand cmdUser = new SqlCommand("UPDATE [dbo].[PeerAdviserConsultations] SET Status = 'CANCELLED' WHERE [PConsultationId] = " + Request.QueryString["aId"]);
         Class2.exe(cmdUser);
         populateListView();
