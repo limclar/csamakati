4044d24c585a98a5423842911d1feb043e9a449e
Jon Pardilla <akcpardilla@gmail.com>
2018-01-27 21:20:02 +0800

    Update StaffDashboard.aspx.cs

--- StaffDashboard.aspx.cs
+++ StaffDashboard.aspx.cs
@@ -29,7 +29,7 @@ public partial class _Default : System.Web.UI.Page
             Session["queryRange"] = "ConsultationDate = CONVERT(date, GETDATE())";
             btnToday.Text = Class2.getSingleData("SELECT COUNT(*) AS ApptToday FROM dbo.PeerAdviserConsultations WHERE ConsultationDate = CONVERT(date, GETDATE()) AND STATUS = 'PENDING' AND TimeEnd IS NULL AND (ConsultationType = 'APPOINTMENT' OR ConsultationType = 'EWP')");
             btnWeek.Text = Class2.getSingleData("SELECT COUNT(*) AS ApptToday FROM dbo.PeerAdviserConsultations WHERE ConsultationDate >= DATEADD(dd, -(DATEPART(dw, GETUTCDATE())-1), GETUTCDATE()) AND ConsultationDate <= DATEADD(dd, 7-(DATEPART(dw, GETUTCDATE())), GETUTCDATE()) AND STATUS = 'PENDING' AND TimeEnd IS NULL AND (ConsultationType = 'APPOINTMENT' OR ConsultationType = 'EWP')");
-            btnMonth.Text = Class2.getSingleData("SELECT COUNT(*) AS ApptToday FROM dbo.PeerAdviserConsultations WHERE ConsultationDate >= GETDATE() AND ConsultationDate<DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())+1, 0)-1 AND STATUS = 'PENDING' AND TimeEnd IS NULL AND (ConsultationType = 'APPOINTMENT' OR ConsultationType = 'EWP')");
+            btnMonth.Text = Class2.getSingleData("SELECT COUNT(*) AS ApptToday FROM dbo.PeerAdviserConsultations WHERE ConsultationDate >= DATEADD(month, DATEDIFF(month, 0, GETUTCDATE()), 0) AND ConsultationDate <= DATEADD(s,-1,dateadd(mm,datediff(m,0,getutcdate())+1,0) AND STATUS = 'PENDING' AND TimeEnd IS NULL AND (ConsultationType = 'APPOINTMENT' OR ConsultationType = 'EWP')");
         }
         
         BindGvData();
@@ -45,7 +45,7 @@ public partial class _Default : System.Web.UI.Page
         else if(btn.ID == "btnWeek")
             Session["queryRange"] = "ConsultationDate >= DATEADD(dd, -(DATEPART(dw, GETUTCDATE())-1), GETUTCDATE()) AND ConsultationDate <= DATEADD(dd, 7-(DATEPART(dw, GETUTCDATE())), GETUTCDATE())";
         else if(btn.ID == "btnMonth")
-            Session["queryRange"] = "ConsultationDate >= GETDATE() AND ConsultationDate<DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())+1, 0)-1";
+            Session["queryRange"] = "ConsultationDate >= DATEADD(month, DATEDIFF(month, 0, GETUTCDATE()), 0) AND ConsultationDate <= DATEADD(s,-1,dateadd(mm,datediff(m,0,getutcdate())+1,0)";
        
        BindGvData();
        BindChart();
