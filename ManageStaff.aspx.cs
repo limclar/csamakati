3dee1af2edf5862d5d8fe53c4537a98ae15e38e2
Jon Pardilla <akcpardilla@gmail.com>
2018-01-28 15:48:30 +0800

    Update ManageStaff.aspx.cs

--- ManageStaff.aspx.cs
+++ ManageStaff.aspx.cs
@@ -34,7 +34,8 @@ public partial class _Default : System.Web.UI.Page
            CheckBox box = ListViewStaff.Items[item].FindControl("CheckBoxID") as CheckBox;
            if (box.Checked)
            {
-              Selected += ListViewStaff.Items[item].Text; 
+              
+              Selected += Proj_NameLabel.Text;
            }
            else
            {
