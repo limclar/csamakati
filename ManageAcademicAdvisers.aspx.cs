e58905377decc4f7a590c6243ffe9489a64016a1
Jon Pardilla <akcpardilla@gmail.com>
2018-01-29 08:15:06 +0800

    Update ManageAcademicAdvisers.aspx.cs

--- ManageAcademicAdvisers.aspx.cs
+++ ManageAcademicAdvisers.aspx.cs
@@ -30,6 +30,17 @@ public partial class _Default : System.Web.UI.Page
             Session["AArchive"] = "NO";
         }
         
+        if(Session["AArchive"] == "NO")
+        {
+            archive.Title = "View Archive";
+            rem.Title = "Move to Archive";
+        }
+        else
+        {
+            archive.Title = "View Active";
+            rem.Title = "Move to Active";
+        }
+        
     }
     
      protected void moveToArchive(object sender, EventArgs e)
