dda98391060d447649ca15879962c49858a25a4b
Jon Pardilla <akcpardilla@gmail.com>
2018-02-03 17:04:04 +0800

    Update ReportGeneration.aspx.cs

--- ReportGeneration.aspx.cs
+++ ReportGeneration.aspx.cs
@@ -7,6 +7,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html;
+using iTextSharp.text.html.simpleparser;
+
 
 public partial class _Default : System.Web.UI.Page
 {
