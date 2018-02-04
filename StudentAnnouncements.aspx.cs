c0c8a9278ddbb40f46d5696c027da2259ce3a5e6
Jon Pardilla <akcpardilla@gmail.com>
2018-02-05 02:45:54 +0800

    Update StudentAnnouncements.aspx.cs

--- StudentAnnouncements.aspx.cs
+++ StudentAnnouncements.aspx.cs
@@ -18,7 +18,7 @@ public partial class _Default : System.Web.UI.Page
             ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('You have been inactive for too long. Please relogin.');window.location ='Out.aspx';", true);
         }
 
-        string cStatus = Class2.getSingleData("SELECT CurrentStatus FROM [dbo].[StudentStatus] WHERE StudentNumber = " + Session["StudentNumber"]" and StudentStatus.SYTerm = '" + Session["SYTerm"] + "'");
+        string cStatus = Class2.getSingleData("SELECT CurrentStatus FROM [dbo].[StudentStatus] WHERE StudentNumber = " + Session["StudentNumber"] + " and StudentStatus.SYTerm = '" + Session["SYTerm"] + "'");
 
         if ((cStatus.Trim() == "EWP" && Class2.getSingleData("SELECT COUNT(*) FROM StudentStatus JOIN PeerAdviserConsultations ON (StudentStatus.StudentNumber = PeerAdviserConsultations.StudentNumber AND StudentStatus.SYTerm = PeerAdviserConsultations.SYTerm) WHERE PeerAdviserConsultations.SYTerm = '" + Session["SYTerm"] + "' AND PeerAdviserConsultations.StudentNumber = " + Session["StudentNumber"] + " AND ConsultationType = 'EWP'") == "0")||(cStatus.Trim() == "EWP" && Class2.getSingleData("SELECT COUNT(*) FROM StudentStatus JOIN EWPRefusal ON (StudentStatus.StudentNumber = EWPRefusal.StudentNumber AND StudentStatus.SYTerm = EWPRefusal.SYTerm) WHERE EWPRefusal.StudentNumber = " + Session["StudentNumber"] + " AND EWPRefusal.SYTerm = '" + Session["SYTerm"] + "'") == "0"))
             ewpAnn.Visible = true;
