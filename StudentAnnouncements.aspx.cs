e91275baebd71e9eea3f32a1b35c9e740eb6e20c
Jon Pardilla <akcpardilla@gmail.com>
2018-02-05 02:39:57 +0800

    Update StudentAnnouncements.aspx.cs

--- StudentAnnouncements.aspx.cs
+++ StudentAnnouncements.aspx.cs
@@ -18,9 +18,11 @@ public partial class _Default : System.Web.UI.Page
             ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('You have been inactive for too long. Please relogin.');window.location ='Out.aspx';", true);
         }
 
-        string cStatus = Class2.getSingleData("SELECT CurrentStatus FROM [dbo].[StudentStatus] WHERE StudentNumber = " + Session["StudentNumber"]);
+        string cStatus = Class2.getSingleData("SELECT CurrentStatus FROM [dbo].[StudentStatus] WHERE StudentNumber = " + Session["StudentNumber"]" and StudentStatus.SYTerm = '" + Session["SYTerm"] + "'");
 
-        if (cStatus.Trim() == "EWP" && Class2.getSingleData("SELECT COUNT(*) FROM StudentStatus JOIN EWPRefusal ON StudentStatus.StudentNumber = EWPRefusal.StudentNumber WHERE EWPRefusal.StudentNumber = " + Session["StudentNumber"] + " AND EWPRefusal.SYTerm = '" + Session["SYTerm"] + "'") == "0")
+        if (cStatus.Trim() == "EWP" && Class2.getSingleData("SELECT COUNT(*) FROM StudentStatus JOIN EWPRefusal ON (StudentStatus.StudentNumber = EWPRefusal.StudentNumber AND StudentStatus.SYTerm = EWPRefusal.SYTerm) WHERE EWPRefusal.StudentNumber = " + Session["StudentNumber"] + " AND EWPRefusal.SYTerm = '" + Session["SYTerm"] + "'") == "0")
+            ewpAnn.Visible = true;
+        else if(cStatus.Trim() == "EWP" && Class2.getSingleData("SELECT COUNT(*) FROM StudentStatus JOIN PeerAdviserConsultations ON (StudentStatus.StudentNumber = PeerAdviserConsultations.StudentNumber AND StudentStatus.SYTerm = PeerAdviserConsultations.SYTerm) WHERE PeerAdviserConsultations.SYTerm = '" + Session["SYTerm"] + "' AND PeerAdviserConsultations.StudentNumber = " + Session["StudentNumber"] + " AND ConsultationType = 'EWP') == "0")
             ewpAnn.Visible = true;
         else
             ewpAnn.Visible = false;
