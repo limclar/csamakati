d057d9eeae53911795a02a901cc4a6e76793f47a
Jon Pardilla <akcpardilla@gmail.com>
2018-02-05 23:14:44 +0800

    Update ManageStudents.aspx.cs

--- ManageStudents.aspx.cs
+++ ManageStudents.aspx.cs
@@ -13,10 +13,7 @@ using System.Text.RegularExpressions;
 using System.Configuration;
 using System.Data.SqlClient; 
 using System.Data;  
-using Microsoft.Office.Interop.Excel;
-using Excel = Microsoft.Office.Interop.Excel;
-using XlReader = Excel;
-using Xl = Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel; 
 
 
 public partial class _Default : System.Web.UI.Page
