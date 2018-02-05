49a36327657155192f436ab141403e045ad5c057
Jon Pardilla <akcpardilla@gmail.com>
2018-02-05 18:11:16 +0800

    Update StaffSessionAttendance.aspx.cs

--- StaffSessionAttendance.aspx.cs
+++ StaffSessionAttendance.aspx.cs
@@ -141,7 +141,7 @@ public partial class _Default : System.Web.UI.Page
                     SqlCommand cmdEnd = new SqlCommand("UPDATE [dbo].[PeerAdviserConsultations] SET Status = 'DONE', [TimeEnd] = convert(char(8), DATEADD(hour,8,GETUTCDATE()), 108) WHERE [PConsultationId] = '" + e.CommandArgument + "'");
                     Class2.exe(cmdEnd);
                     populateListView();
-                    ScriptManager.RegisterStartupScript(this, typeof(string), "Message", "if(confirm('Do you really want to end the consultation?')){alert('Consultation is now done! Please take the evaluation.'); window.open('StudentSessionEvaluation.aspx?aId=" + e.CommandArgument + "','_blank'); }else{}",true);
+                    ScriptManager.RegisterStartupScript(Page, Page.GetType(), "Message", "if(confirm('Do you really want to end the consultation?')){alert('Consultation is now done! Please take the evaluation.'); window.open('StudentSessionEvaluation.aspx?aId=" + e.CommandArgument + "','_blank'); }else{}",true);
                 }
                 else
                 {
