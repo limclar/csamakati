7768f58c0e5bf7d592d0e34bdb1dd33a95ff066b
Jon Pardilla <akcpardilla@gmail.com>
2018-02-05 15:17:26 +0800

    Update StaffSessionAttendance.aspx.cs

--- StaffSessionAttendance.aspx.cs
+++ StaffSessionAttendance.aspx.cs
@@ -181,7 +181,6 @@ public partial class _Default : System.Web.UI.Page
             else if(e.CommandName == "UpdateCon")
             {
                 Session["eArg"] = e.CommandArgument;
-                fillDDLPA();
                 ScriptManager.RegisterStartupScript(this, typeof(string), "uniqueKey", "div_show()", true);
             }
             else if(e.CommandName == "CancelCon")
