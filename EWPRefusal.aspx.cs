a53ac872279022e3aeee0cd1902aa260f6fb8c51
Jon Pardilla <akcpardilla@gmail.com>
2018-02-06 23:56:55 +0800

    Update EWPRefusal.aspx.cs

--- EWPRefusal.aspx.cs
+++ EWPRefusal.aspx.cs
@@ -37,15 +37,19 @@ public partial class _Default : System.Web.UI.Page
 
     protected void btnsubmitEWP_Click(object sender, EventArgs e)
     {
-        if (CheckBoxList1.SelectedIndex == 6)
-            reason = txtOthers.Text;
-        else
-            reason = CheckBoxList1.Text;
+        try
+        {
+            if (CheckBoxList1.SelectedIndex == 6)
+                reason = txtOthers.Text;
+            else
+                reason = CheckBoxList1.Text;
 
-        SqlCommand cmdEWPRefuse = new SqlCommand("INSERT INTO[dbo].[EWPRefusal] VALUES( " + Session["StudentNumber"] + ", '" + DateTime.Now.ToString().Split(' ')[0] + "', '" + reason + "', '" + Session["SYTerm"] + "')");
-        Class2.exe(cmdEWPRefuse);
-        this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You have submitted your refusal form. Thank you.'); window.location ='StudentAnnouncements.aspx';", true);
-
-        
+            SqlCommand cmdEWPRefuse = new SqlCommand("INSERT INTO[dbo].[EWPRefusal] VALUES( " + Session["StudentNumber"] + ", '" + DateTime.Now.ToString().Split(' ')[0] + "', '" + reason + "', '" + Session["SYTerm"] + "')");
+            Class2.exe(cmdEWPRefuse);
+            this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You have submitted your refusal form. Thank you.'); window.location ='StudentAnnouncements.aspx';", true);
+        }
+        catch(Exception ex)
+        {
+        }
     }
 }
