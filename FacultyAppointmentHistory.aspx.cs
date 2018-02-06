4b7ba414f196d5f4c454c7d8a4b76ba64dccacf1
Jon Pardilla <akcpardilla@gmail.com>
2018-02-06 23:59:19 +0800

    Update FacultyAppointmentHistory.aspx.cs

--- FacultyAppointmentHistory.aspx.cs
+++ FacultyAppointmentHistory.aspx.cs
@@ -20,19 +20,30 @@ public partial class _Default : System.Web.UI.Page
             ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('You have been inactive for too long. Please relogin.');window.location ='Out.aspx';", true);
         }
 
+        try
+        {
+            SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviser.UserId = " + Session["UserId"]);
 
-        SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviser.UserId = " + Session["UserId"]);
-
-        ListViewAHistory.DataSource = Class2.getDataSet(cmd);
-        ListViewAHistory.DataBind();
+            ListViewAHistory.DataSource = Class2.getDataSet(cmd);
+            ListViewAHistory.DataBind();
+        }
+        catch(Exception ex)
+        {
+        }
     }
 
     public void sortByAction(string act)
     {
-        SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviser.UserId = " + Session["UserId"] + " and dbo.AcademicAdviserConsultations.ActionTaken = '" + act + "';");
+        try
+        {
+            SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviser.UserId = " + Session["UserId"] + " and dbo.AcademicAdviserConsultations.ActionTaken = '" + act + "';");
 
-        ListViewAHistory.DataSource = Class2.getDataSet(cmd);
-        ListViewAHistory.DataBind();
+            ListViewAHistory.DataSource = Class2.getDataSet(cmd);
+            ListViewAHistory.DataBind();
+        }
+        catch(Exception ex)
+        {
+        }
     }
 
     public void sortByFFU(object sender, EventArgs e)
@@ -42,10 +53,16 @@ public partial class _Default : System.Web.UI.Page
 
     public void sortByRef(object sender, EventArgs e)
     {
-        SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviserConsultations.ActionTaken <> 'For Follow Up' and dbo.AcademicAdviserConsultations.ActionTaken <> 'Resolved' and dbo.AcademicAdviser.UserId = " + Session["UserId"]);
+        try
+        {
+            SqlCommand cmd = new SqlCommand("SELECT dbo.AcademicAdviserConsultations.ConsultationDateTime, (SELECT dbo.Student.StudentName FROM Student WHERE Student.StudentNumber = AcademicAdviserConsultations.StudentNumber) as [Student Name], dbo.AcademicAdviserConsultations.ConsultationCode, dbo.AcademicAdviserConsultations.NatureOfAdvising, dbo.AcademicAdviserConsultations.ActionTaken FROM dbo.AcademicAdviserConsultations INNER JOIN dbo.AcademicAdviser ON dbo.AcademicAdviserConsultations.AAdviserId = dbo.AcademicAdviser.AAdviserId WHERE dbo.AcademicAdviserConsultations.Status = 'DONE' and dbo.AcademicAdviserConsultations.ActionTaken <> 'For Follow Up' and dbo.AcademicAdviserConsultations.ActionTaken <> 'Resolved' and dbo.AcademicAdviser.UserId = " + Session["UserId"]);
 
-        ListViewAHistory.DataSource = Class2.getDataSet(cmd);
-        ListViewAHistory.DataBind();
+            ListViewAHistory.DataSource = Class2.getDataSet(cmd);
+            ListViewAHistory.DataBind();
+        }
+        catch(Exception ex)
+        {
+        }
         
     }
 
