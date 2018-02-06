afe7bf3264447b4d05f515e86dd5f069a8316194
limclar <35856597+limclar@users.noreply.github.com>
2018-02-06 12:58:40 +0800

    Update ManageStudents.aspx.cs

--- ManageStudents.aspx.cs
+++ ManageStudents.aspx.cs
@@ -106,8 +106,56 @@ public partial class _Default : System.Web.UI.Page
     
     protected void btnUpload_Click(object sender, EventArgs e)
     {   
-        string CurrentFilePath = Path.GetFullPath(FileUpload1.PostedFile.FileName);  
-        InsertExcelRecords(CurrentFilePath);   
+        try
+       {
+            string path = string.Concat(Server.MapPath("~/" + FileUpload1.PostedFile.FileName));
+            if(File.Exists(path))
+            {
+               File.Delete(path);
+            }
+            else
+            {
+              FileUpload1.SaveAs(path);
+            }
+            string excelConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;Persist Security Info=False";
+            OleDbConnection excelConnection =new OleDbConnection(excelConnectionString);
+
+            OleDbCommand cmd = new OleDbCommand("Select * from [Sheet1$]",excelConnection);
+            excelConnection.Open();
+           
+            OleDbDataReader dReader = cmd.ExecuteReader();
+            SqlBulkCopy sqlBulk = new SqlBulkCopy(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            
+            
+            if(ddlTable.SelectedValue == "GRADES")
+            {
+              sqlBulk.DestinationTableName = "[StudentGrades]";
+              sqlBulk.WriteToServer(dReader);
+            }
+            else if(ddlTable.SelectedValue == "STATUS")
+            {
+                OleDbCommand cmdSS = new OleDbCommand("SELECT StudentNumber, Program, YearLvl, SYTerm, AcademicStatus, (SELECT SWITCH(AcademicStatus = 'ACADEMIC GOOD STANDING','PEER',AcademicStatus = 'ACADEMIC WARNING STATUS','EWP',AcademicStatus = 'ACADEMIC PROBATIONARY STATUS','CARE',AcademicStatus = 'ACADEMIC FINAL PROBATIONARY STATUS','CARE') from [Sheet1$]) AS CurrentStatus, NOW(), LastEnrolled, AcademicAdviser from [Sheet1$]", excelConnection);
+                OleDbDataReader drSS = cmdSS.ExecuteReader();
+                sqlBulk.DestinationTableName = "[StudentStatus]";
+                sqlBulk.WriteToServer(drSS);
+            }
+            else
+            {
+              sqlBulk.DestinationTableName = "[Student]";
+              sqlBulk.WriteToServer(dReader);
+              OleDbCommand cmdCnt = new OleDbCommand("select count(*) from [Sheet1$]", excelConnection);
+              cmdCnt.Connection.Open();
+              Session["rowCount"] = cmdCnt.ExecuteScalar().ToString();
+            }
+            excelConnection.Close();
+             ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Importing data from excel successful!');window.location ='ManageStudents.aspx';", true);
+           
+       }
+       catch(Exception ex)
+       {
+           Label1.Text = ex.Message;
+            //ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('Failed to import data!');window.location ='ManageStudents.aspx';", true);
+       } 
     }
 
     protected void btnSave_Click(object sender, EventArgs e)
