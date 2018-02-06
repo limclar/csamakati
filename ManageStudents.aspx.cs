86d80ef471b5eb3b470993bccb52844b1d5116ad
limclar <35856597+limclar@users.noreply.github.com>
2018-02-06 15:21:16 +0800

    Update ManageStudents.aspx.cs

--- ManageStudents.aspx.cs
+++ ManageStudents.aspx.cs
@@ -18,6 +18,15 @@ public partial class _Default : System.Web.UI.Page
 {
    protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            checkUsertype.filter("STAFF", Session["UserType"].ToString());
+        }
+        catch(Exception ex)
+        {
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "alert", "alert('You have been inactive for too long. Please relogin.');window.location ='Out.aspx';", true);
+        }
+        
         if(!IsPostBack)
         {
 
